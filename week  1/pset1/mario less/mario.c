d5de0ddc7f2d0895f20ac5323bc0688683c13e24
Joffrey Nolting <joffrey.nolting@gmail.com>
2021-10-16 08:04:43 +0200

    Create mario.c

---
+++ week  1/pset1/mario less/mario.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Prompt the user for the height of the pyramid
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height > 8 || height <= 0);
+
+    // create the pyramid
+    // each row
+    for (int i = 0; i < height; i++)
+    {
+        // each line
+        printf("%*s", height - i - 1, ""); // trick to save a for loop
+
+        for (int k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
