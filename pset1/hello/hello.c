fc337c71527df97535f1e57c80ba9f829b42192f
Joffrey Nolting <joffrey.nolting@gmail.com>
2021-10-16 07:59:23 +0200

    Delete pset1 directory

--- pset1/hello/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    // asks the user for its name, so it then can greet the user
-    string name = get_string("What is your name?\n");
-    printf("Hello, %s\n", name);
-}
